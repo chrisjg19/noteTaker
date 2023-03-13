f7ae198277cdb8ba3f42c2b228c80a8ca97fb7b1
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-03-13 16:57:55 -0500

    added the get routes

--- server.js
+++ server.js
@@ -9,3 +9,15 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
+app.get('/api/notes', (req, res) =>{
+    res.json(notesData)
+});
+app.get('/api/notes/:id', (req, res) => {
+    res.json(notesData[req.params.id])
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
