7b5468f1fa24857feafb8bdd5fe93e8602d7b303
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-03-13 16:17:05 -0500

    added the middleware from expresss

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const notesData = require('./db/db.json');
 const app = express();
 const port = process.env.Port || 3001;
 
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
